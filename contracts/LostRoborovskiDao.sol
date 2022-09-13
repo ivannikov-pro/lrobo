bbd5dc49ceb7e42798c96f1459a917e97a13e0c8
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-09-13 16:05:59 +0400

    Create LostRoborovskiDao.sol

---
+++ contracts/LostRoborovskiDao.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+/**
+ *
+ */
+contract LostRoborovskiDao {
+    address public nft;
+
+    constructor(address nft_) {
+        nft = nft_;
+    }
+}
